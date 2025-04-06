6031f1f24977e4ffac6c58ebf2d8c324560ebcfb
Dominik Pozniak <domiprzemhq@gmail.com>
2025-04-07 00:23:42 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">🚀 Finpilot App is Live</h1>
+    </div>
+  );
+}
